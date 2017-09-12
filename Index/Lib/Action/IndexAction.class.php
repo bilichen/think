e3c9b9f0a18b2633365659f903b2078b52d28306
Administrator <1880161@qq.com>
2017-09-13 01:22:42 +0800

    11

--- Index/Lib/Action/IndexAction.class.php
+++ Index/Lib/Action/IndexAction.class.php
@@ -6,6 +6,32 @@ class IndexAction extends Action {
 //        $result = $db->select();
 //        dump($result);
 //        echo C('username');
-        test();
+//        test();
+//        p(I('uid'));die;
+        //先获取变量数据，再显示页面，从而带着数据去
+        $this->assign('wish',M('wish')->select())->display();
+    }
+    public function handle(){
+        //先判断是否post提交过来，是才正确运行，否则返回404页面即可
+        if(!IS_POST){
+            _404('不能直接访问表单处理函数',U('index'));
+        }else{
+            $data = array(
+                'username' => I('username','',htmlspecialchars()),
+                'content' => I('content','',htmlspecialchars()),
+                'time' => time()
+            );
+            //thinkphp删除数据，要加where语句 where(array('id' => array('gt',0))
+//            $result  = M('wish')->where('id > 0')->delete();
+
+           if(M('wish')->data($data)->add()){
+               $this->success('发布成功','index');
+           }else{
+               $this->error('发布失败，请重试');
+           }
+
+        }
+
+
     }
 }
